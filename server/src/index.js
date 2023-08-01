5e33082a34ad69f77bea22407d1ad5b712236541
Kian Yee <kianyee.lim@hotmail.com>
2023-08-01 22:08:36 +0800

    fix(auth): bug

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,10 @@ const authRoute = require('./routes/auth/auth.controller')
 const app = express()
 
 app.use(corsWithOptions)
+app.use(express.json())
 
 // Routes
-app.use('auth', authRoute)
+app.use('/auth', authRoute)
 
 // TODO: refactor to env var
 const port = 8000
